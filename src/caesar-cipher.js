650430ad4128ab0270e2dd1257814c2c7416564c
Eleazar Meza <bairesdev_emeza@iseatz.com>
2020-02-10 19:39:35 -0400

    Added support upcased letters in Caesar Cipher

--- src/caesar-cipher.js
+++ src/caesar-cipher.js
@@ -3,9 +3,10 @@ const CaesarCipher = (() => {
     if (key === 0) { return message; }
     if (key < 0) { key = 26 + key; }
 
-    return message.replace(/\w/g, c => String.fromCharCode(
-      ((c.charCodeAt() - 97 + op * key) % 26) + 97,
-    ));
+    return message.replace(/[a-z]/ig, c => {
+      const offset = c.charCodeAt() < 97 ? 65 : 97;
+      return String.fromCharCode(((c.charCodeAt() - offset + op * key) % 26) + offset);
+    });
   };
 
   const encrypt = (message, key) => caesar(message, key);
--- test/caesar-cipher.test.js
+++ test/caesar-cipher.test.js
@@ -1,11 +1,18 @@
 const CaesarCipher = require('../src/caesar-cipher');
 
-test('it correctly encrypts a message', () => {
-  expect(CaesarCipher.encrypt('defend the east wall of the castle', 1))
-    .toBe('efgfoe uif fbtu xbmm pg uif dbtumf');
-});
+describe('#encrypt', () => {
+  test('it correctly encrypts a message', () => {
+    expect(CaesarCipher.encrypt('defend the east wall of the castle', 1))
+      .toBe('efgfoe uif fbtu xbmm pg uif dbtumf');
+  });
 
-test('it correctly decrypts a message', () => {
-  expect(CaesarCipher.decrypt('efgfoe uif fbtu xbmm pg uif dbtumf', 1))
-    .toBe('defend the east wall of the castle');
+  test('it correctly deals with upcased characters', () => {
+    expect(CaesarCipher.encrypt('DEFEND the east wall of THE CASTLE', 1))
+      .toBe('EFGFOE uif fbtu xbmm pg UIF DBTUMF');
+  });
+
+  test('it ignores punctuation and other symbols', () => {
+    expect(CaesarCipher.encrypt('DEFEND!!!, the east wall of THE CASTLE!!!.', 1))
+      .toBe('EFGFOE!!!, uif fbtu xbmm pg UIF DBTUMF!!!.');
+  });
 });
