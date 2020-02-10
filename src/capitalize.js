f384ae57353ee4b5ea39bc1059ecbcf3307b1187
Eleazar Meza <bairesdev_emeza@iseatz.com>
2020-02-10 17:26:55 -0400

    Finished capitalize implementation with test coverage

--- src/capitalize.js
+++ src/capitalize.js
@@ -1,4 +1,7 @@
 const capitalize = (string) => {
+  if (string.length === 0) {
+    return string;
+  }
   return string[0].toUpperCase() + string.slice(1);
 };
 
--- test/capitalize.test.js
+++ test/capitalize.test.js
@@ -7,3 +7,11 @@ test('it capitalizes a string', () => {
 test('it does not blow up with an empty string', () => {
   expect(capitalize('')).toBe('');
 });
+
+test('it does not blow up with a single char string', () => {
+  expect(capitalize('a')).toBe('A');
+});
+
+test('it does not change an already capitalized string', () => {
+  expect(capitalize('ALREADY')).toBe('ALREADY');
+});
