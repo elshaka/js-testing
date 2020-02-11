253744c63eabd8aa048bbb655739a3c16c9c3599
Aaron-RN <aaron.rory.newbold@gmail.com>
2020-02-11 13:23:20 -0500

    removed some redundant code in the tests file for array-analysis

--- test/array-analysis.test.js
+++ test/array-analysis.test.js
@@ -1,28 +1,9 @@
 const analyze = require('../src/array-analysis');
 
-const array = [1, 8, 3, 4, 2, 6];
-const obj = analyze(array);
-
-test('it analyses an array and returns an object', () => {
+test('it analyses an array and returns an object with values of an average, min, max and length', () => {
   expect(analyze([1, 8, 3, 4, 2, 6])).toStrictEqual({
     average: 4, min: 1, max: 8, length: 6,
   });
-});
-
-test('object returned has an average property', () => {
-  expect(obj.average).toBe(4);
-});
-
-test('object returned has an min property', () => {
-  expect(obj.min).toBe(1);
-});
-
-test('object returned has an max property', () => {
-  expect(obj.max).toBe(8);
-});
-
-test('object returned has an length property', () => {
-  expect(obj.length).toBe(6);
 });
 
 test('throws an error if array given is empty', () => {
