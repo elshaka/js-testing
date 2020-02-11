e844472c39d93e855b51d83aa13d334b75c1e9c6
Aaron-RN <aaron.rory.newbold@gmail.com>
2020-02-11 13:24:28 -0500

    used toEqual instead of toStrictEqual in test

--- test/array-analysis.test.js
+++ test/array-analysis.test.js
@@ -1,7 +1,7 @@
 const analyze = require('../src/array-analysis');
 
 test('it analyses an array and returns an object with values of an average, min, max and length', () => {
-  expect(analyze([1, 8, 3, 4, 2, 6])).toStrictEqual({
+  expect(analyze([1, 8, 3, 4, 2, 6])).toEqual({
     average: 4, min: 1, max: 8, length: 6,
   });
 });
